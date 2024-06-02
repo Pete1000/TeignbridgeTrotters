0f69653fa8557186977ede09d5bdc345fcc3ae22
pete <astonp@yahoo.com>
2024-06-02 12:51:08 +0100

    Initial Commit

---
+++ TeignbridgeTrotters.ResultDownloader/TeignbridgeTrotters.ResultDownloader/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft;
+using Microsoft.Extensions.Configuration;
+using TeignbridgeTrotters.ResultDownloader;
+
+var configuration = new ConfigurationBuilder()
+     .AddJsonFile($"appsettings.json");
+
+ProgramSettings programSettings = Requires.NotNull(configuration.Build().GetRequiredSection("ProgramSettings")!.Get<ProgramSettings>()!);
+
+while (1 == 1)
+{
+    using (var client = new HttpClient())
+    {
+        var response = await client.GetStreamAsync(programSettings.SourceFileName);
+        using FileStream outputFileStream = new(programSettings.DestinationFileName!, FileMode.Create);
+        await response.CopyToAsync(outputFileStream);
+    }
+    
+    Console.WriteLine($"{DateTime.Now:T} - Downloaded file. Waiting for {programSettings.WaitTimeSeconds} seconds");
+    Thread.Sleep(programSettings.WaitTimeSeconds * 1000);
+};
---
+++ TeignbridgeTrotters.ResultDownloader/TeignbridgeTrotters.ResultDownloader/ProgramSettings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeignbridgeTrotters.ResultDownloader
+{
+    public class ProgramSettings
+    {
+        public int WaitTimeSeconds { get; set; }
+        public string? SourceFileName { get; set; }
+        public string? DestinationFileName { get; set; }
+    }
+}
