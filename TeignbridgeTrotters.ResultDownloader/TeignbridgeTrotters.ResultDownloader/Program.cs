a711bf3a99e634a3e8a1ec05f784f4fc4bfd84af
pete <astonp@yahoo.com>
2024-06-09 15:05:32 +0100

    Use MD5 for comparison

--- TeignbridgeTrotters.ResultDownloader/TeignbridgeTrotters.ResultDownloader/Program.cs
+++ TeignbridgeTrotters.ResultDownloader/TeignbridgeTrotters.ResultDownloader/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft;
 using Microsoft.Extensions.Configuration;
+using System.Security.Cryptography;
 using System.Text;
 using TeignbridgeTrotters.ResultDownloader;
 
@@ -8,7 +9,8 @@ var configuration = new ConfigurationBuilder()
 
 ProgramSettings programSettings = Requires.NotNull(configuration.Build().GetRequiredSection("ProgramSettings")!.Get<ProgramSettings>()!);
 string message = string.Empty;
-long previousStreamLength = 0;
+MD5 md5 = MD5.Create();
+string previousMd5string = string.Empty;
 
 while (1 == 1)
 {
@@ -19,19 +21,21 @@ while (1 == 1)
             var responseStream = await client.GetStreamAsync(programSettings.SourceFileName);
             var memoryStream = new MemoryStream();
             responseStream.CopyTo(memoryStream);
-            if (memoryStream.Length != previousStreamLength)
+            memoryStream.Position = 0;
+            var md5string = Convert.ToHexString(md5.ComputeHash(memoryStream));
+            if (md5string != previousMd5string)
             {
                 memoryStream.Position = 0;
                 using (FileStream outputFileStream = new(programSettings.DestinationFileName!, FileMode.Create))
                 {
                     await memoryStream.CopyToAsync(outputFileStream);
                 }
-                previousStreamLength = memoryStream.Length;
+                previousMd5string = md5string;
                 message = $"Downloaded {programSettings.SourceFileName} to {programSettings.DestinationFileName}";
             }
             else
             {
-                message = $"Not downloading {programSettings.SourceFileName} as length has not changed ({previousStreamLength})";
+                message = $"Not downloading {programSettings.SourceFileName} as md5 has not changed ({previousMd5string})";
             }
         }
         catch (Exception ex)
