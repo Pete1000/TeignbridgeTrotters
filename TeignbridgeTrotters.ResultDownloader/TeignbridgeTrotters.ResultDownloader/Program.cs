65ec13dbc616b7be89b249c7e9222d51f368ab05
pete <astonp@yahoo.com>
2024-06-09 14:03:29 +0100

    Improve error handling

--- TeignbridgeTrotters.ResultDownloader/TeignbridgeTrotters.ResultDownloader/Program.cs
+++ TeignbridgeTrotters.ResultDownloader/TeignbridgeTrotters.ResultDownloader/Program.cs
@@ -6,16 +6,25 @@ var configuration = new ConfigurationBuilder()
      .AddJsonFile($"appsettings.json");
 
 ProgramSettings programSettings = Requires.NotNull(configuration.Build().GetRequiredSection("ProgramSettings")!.Get<ProgramSettings>()!);
+string message = string.Empty;
 
 while (1 == 1)
 {
     using (var client = new HttpClient())
     {
-        var response = await client.GetStreamAsync(programSettings.SourceFileName);
-        using FileStream outputFileStream = new(programSettings.DestinationFileName!, FileMode.Create);
-        await response.CopyToAsync(outputFileStream);
+        try
+        {
+            var response = await client.GetStreamAsync(programSettings.SourceFileName);
+            using FileStream outputFileStream = new(programSettings.DestinationFileName!, FileMode.Create);
+            await response.CopyToAsync(outputFileStream);
+            message = $"Downloaded {programSettings.SourceFileName} to {programSettings.DestinationFileName}";
+        } 
+        catch (Exception ex)
+        {
+            message = $"Error: {ex.Message}";
+        }
     }
     
-    Console.WriteLine($"{DateTime.Now:T} - Downloaded {programSettings.SourceFileName} to {programSettings.DestinationFileName}. Waiting for {programSettings.WaitTimeSeconds} seconds");
+    Console.WriteLine($"{DateTime.Now:T} - {message} - Waiting for {programSettings.WaitTimeSeconds} seconds");
     Thread.Sleep(programSettings.WaitTimeSeconds * 1000);
 };
