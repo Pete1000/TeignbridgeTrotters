5cdf7e0b198b08883da79f3a4660254fccab0066
pete <astonp@yahoo.com>
2024-06-09 14:56:53 +0100

    Don't download the file if it hasn't changed length

--- TeignbridgeTrotters.ResultDownloader/TeignbridgeTrotters.ResultDownloader/Program.cs
+++ TeignbridgeTrotters.ResultDownloader/TeignbridgeTrotters.ResultDownloader/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft;
 using Microsoft.Extensions.Configuration;
+using System.Text;
 using TeignbridgeTrotters.ResultDownloader;
 
 var configuration = new ConfigurationBuilder()
@@ -7,6 +8,7 @@ var configuration = new ConfigurationBuilder()
 
 ProgramSettings programSettings = Requires.NotNull(configuration.Build().GetRequiredSection("ProgramSettings")!.Get<ProgramSettings>()!);
 string message = string.Empty;
+long previousStreamLength = 0;
 
 while (1 == 1)
 {
@@ -14,17 +16,30 @@ while (1 == 1)
     {
         try
         {
-            var response = await client.GetStreamAsync(programSettings.SourceFileName);
-            using FileStream outputFileStream = new(programSettings.DestinationFileName!, FileMode.Create);
-            await response.CopyToAsync(outputFileStream);
-            message = $"Downloaded {programSettings.SourceFileName} to {programSettings.DestinationFileName}";
-        } 
+            var responseStream = await client.GetStreamAsync(programSettings.SourceFileName);
+            var memoryStream = new MemoryStream();
+            responseStream.CopyTo(memoryStream);
+            if (memoryStream.Length != previousStreamLength)
+            {
+                memoryStream.Position = 0;
+                using (FileStream outputFileStream = new(programSettings.DestinationFileName!, FileMode.Create))
+                {
+                    await memoryStream.CopyToAsync(outputFileStream);
+                }
+                previousStreamLength = memoryStream.Length;
+                message = $"Downloaded {programSettings.SourceFileName} to {programSettings.DestinationFileName}";
+            }
+            else
+            {
+                message = $"Not downloading {programSettings.SourceFileName} as length has not changed ({previousStreamLength})";
+            }
+        }
         catch (Exception ex)
         {
             message = $"Error: {ex.Message}";
         }
     }
-    
+
     Console.WriteLine($"{DateTime.Now:T} - {message} - Waiting for {programSettings.WaitTimeSeconds} seconds");
     Thread.Sleep(programSettings.WaitTimeSeconds * 1000);
 };
